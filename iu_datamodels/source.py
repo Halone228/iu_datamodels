a897723bc169de2795d1d020a6089f3e5e00d02f
halone <haloneprog@gmail.com>
2024-04-08 11:41:20 +0300

    [p] Patch update, source more inheritance

--- iu_datamodels/source.py
+++ iu_datamodels/source.py
@@ -2,21 +2,26 @@ from .base import BaseModel
 from .vein import Vein
 
 
-class SourceShort(BaseModel):
-    id: int
+class SourceBase(BaseModel):
     uname: str
     metadata: dict
 
 
-class Source(SourceShort):
+class SourceShort(BaseModel):
     vein_id: int
 
 
-class SourceFull(SourceShort):
+class Source(SourceShort):
+    id: int
+
+
+class SourceFull(SourceBase):
+    id: int
     vein: Vein
 
 
 __all__ = [
+    'SourceBase'
     'Source',
     'SourceShort',
     'SourceFull'
